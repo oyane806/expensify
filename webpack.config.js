4e7d0df0b2cb589a3a5bf33c58ab0de0b1d39035
oyane806 <oceane.velon@gmail.com>
2019-06-03 19:30:06 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+// entry => output
+
+const path = require("path");
+
+module.exports = {
+	entry: "./src/playground/redux-expensify.jsx",
+	output: {
+		path: path.join(__dirname, "public"), // Absolute
+		filename: "bundle.js"
+	},
+	module: {
+		rules: [{
+			loader: "babel-loader",
+			test: /\.jsx$/,
+			exclude: /node_modules/
+		}, {
+			test: /\.s?css$/,
+			use: [
+			"style-loader",
+			"css-loader",
+			"sass-loader"
+			]
+		}, {
+			loader: "url-loader?limit=100000",
+			test: /\.png$/,
+		}]
+	},
+	devtool: "cheap-module-eval-source-map", // Source map
+	devServer: {
+		contentBase: path.join(__dirname, "public"),
+		historyApiFallback: true
+	}
+};
