fa70056deecb9dac02be4a308bc44b23686b242a
oyane806 <oceane.velon@gmail.com>
2019-06-17 19:06:47 -0700

    Setup webpack production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,55 @@
 // entry => output
 
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-	entry: "./src/app.jsx",
-	output: {
-		path: path.join(__dirname, "public"), // Absolute
-		filename: "bundle.js"
-	},
-	module: {
-		rules: [{
-			loader: "babel-loader",
-			test: /\.jsx$/,
-			exclude: /node_modules/
-		}, {
-			test: /\.s?css$/,
-			use: [
-			"style-loader",
-			"css-loader",
-			"sass-loader"
-			]
-		}, {
-			loader: "url-loader?limit=100000",
-			test: /\.png$/,
-		}]
-	},
-	devtool: "cheap-module-eval-source-map", // Source map
-	devServer: {
-		contentBase: path.join(__dirname, "public"),
-		historyApiFallback: true
-	}
+module.exports = (env) => {
+    const isProduction = (env === "production");
+    const CSSExtract = new MiniCssExtractPlugin({
+        filename: "styles.css"
+    });
+    // We want to extract the css into this file.
+
+    return {
+        entry: "./src/app.jsx",
+        output: {
+            path: path.join(__dirname, "public"), // Absolute
+            filename: "bundle.js"
+        },
+        module: {
+            rules: [{
+                loader: "babel-loader",
+                test: /\.jsx$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }, {
+                loader: "url-loader?limit=100000",
+                test: /\.png$/,
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? "source-map" : "inline-source-map", // Source map
+        devServer: {
+            contentBase: path.join(__dirname, "public"),
+            historyApiFallback: true
+        }
+    };
 };
